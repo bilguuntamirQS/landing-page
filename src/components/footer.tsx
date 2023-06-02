4d3f8694c8b42551db11e7e7e4bc49764a31f66c
Bilguuntamir <bilguuntamir@qualsmart.com>
2023-06-02 15:11:27 +0800

    change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,24 +4,24 @@ import { type FC } from "react";
 import facebookSvg from "~/assets/facebook-blue.svg";
 import twitterSvg from "~/assets/twitter-blue.svg";
 import instagramSvg from "~/assets/instagram-blue.svg";
-import { MotionLink, fadeInMotion, fadeInUpMotion } from "~/utils/animation";
 import { motion } from "framer-motion";
 
 const Footer: FC = () => {
   return (
-    <section className="relative">
-      <div className="absolute inset-0 ml-auto hidden w-1/2 bg-blueGray-50 lg:block"></div>
-      <div className="container">
-        <div className="relative -mx-4 flex flex-wrap">
-          <div className="mx-auto w-full max-w-lg px-4 pt-20 text-center lg:mx-0 lg:w-1/2 lg:max-w-full lg:text-left">
-            <MotionLink
-              variants={fadeInUpMotion}
-              custom={0.1}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: true }}
+    <section className="py-20">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+        }}
+        className="container"
+      >
+        <div className="-mx-3 mb-12 flex flex-wrap text-center lg:mb-20 lg:text-left">
+          <div className="mb-6 w-full px-3 lg:mb-0 lg:w-1/5">
+            <Link
               href="/"
-              className="mx-auto inline-block text-3xl font-semibold leading-none lg:ml-0"
+              className="mx-auto inline-block text-3xl font-semibold leading-none lg:mx-0"
             >
               <Image
                 className="h-10"
@@ -30,224 +30,50 @@ const Footer: FC = () => {
                 width={125.8}
                 alt="logo"
               />
-            </MotionLink>
-            <div className="mb-16 mt-12">
-              <motion.p
-                variants={fadeInUpMotion}
-                custom={0.1}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                className="mb-4 text-center text-xs text-blueGray-400 lg:text-left"
-              >
-                Subscribe and stay fully connected with product.
-              </motion.p>
-              <motion.div
-                variants={fadeInUpMotion}
-                custom={0.3}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                className="flex flex-wrap items-center lg:flex-nowrap"
-              >
-                <div className="mb-3 flex w-full rounded bg-blueGray-50 px-4 lg:mb-0 lg:w-2/3">
-                  <svg
-                    className="my-auto h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                  </svg>
-                  <input
-                    className="w-full bg-blueGray-50 py-4 pl-3 text-xs text-blueGray-400 outline-none"
-                    type="text"
-                    placeholder="Type your e-mail"
-                  />
-                </div>
-                <motion.button
-                  variants={fadeInMotion}
-                  whileHover="hover"
-                  className="hover-up-2 w-full rounded bg-blue-400 px-8 py-4 text-xs font-semibold leading-none text-white hover:bg-blue-500 lg:ml-6 lg:w-auto"
-                  type="submit"
-                >
-                  Sign Up
-                </motion.button>
-              </motion.div>
-            </div>
-            <motion.div
-              variants={fadeInUpMotion}
-              custom={0.5}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: true }}
-              className="hidden lg:block"
-            >
-              <p className="text-xs text-blueGray-400">
-                © 2021. All rights reserved.
-              </p>
-              <div className="-mx-2 mt-4">
-                <Link className="inline-block px-2" href="#">
-                  <Image src={facebookSvg as string} alt="facebook-logo" />
-                </Link>
-                <Link className="inline-block px-2" href="#">
-                  <Image src={twitterSvg as string} alt="twitter-logo" />
-                </Link>
-                <Link className="inline-block px-2" href="#">
-                  <Image src={instagramSvg as string} alt="instagram-logo" />
-                </Link>
-              </div>
-            </motion.div>
+            </Link>
           </div>
-          <div className="relative w-full px-4 pt-40 lg:w-1/2 lg:pl-12">
-            <div className="mb-12 flex flex-row flex-wrap justify-around">
-              <motion.div
-                variants={fadeInUpMotion}
-                custom={0.1}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                className="mb-12 w-1/2 lg:w-auto"
-              >
-                <h5 className="mb-6 font-heading text-lg font-bold">About</h5>
-                <ul className="space-y-3">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      About us
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Portfolio
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-              </motion.div>
-              <motion.div
-                variants={fadeInUpMotion}
-                custom={0.3}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                className="mb-12 w-1/2 lg:w-auto"
-              >
-                <h5 className="mb-6 font-heading text-lg font-bold">
-                  Quick Links
-                </h5>
-                <ul className="space-y-3">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Team
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Pricing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Tesimonials
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Faqs
-                    </Link>
-                  </li>
-                </ul>
-              </motion.div>
-              <motion.div
-                variants={fadeInUpMotion}
-                custom={0.5}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }}
-                className="mb-12 w-1/2 lg:w-auto"
-              >
-                <h5 className="mb-6 font-heading text-lg font-bold">Blog</h5>
-                <ul className="space-y-3">
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Business
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Marketing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="text-lg text-blueGray-400 hover:text-blueGray-500"
-                    >
-                      Single
-                    </Link>
-                  </li>
-                </ul>
-              </motion.div>
-            </div>
-            <div className="text-center lg:hidden">
-              <p className="text-xs text-blueGray-400">
-                © 2021. All rights reserved.
-              </p>
-              <div className="-mx-2 mt-4">
-                <Link className="inline-block px-2" href="#">
-                  <Image src={facebookSvg as string} alt="facebook-logo" />
-                </Link>
-                <Link className="inline-block px-2" href="#">
-                  <Image src={twitterSvg as string} alt="twitter-logo" />
-                </Link>
-                <Link className="inline-block px-2" href="#">
-                  <Image src={instagramSvg as string} alt="instagram-logo" />
-                </Link>
-              </div>
-            </div>
+          <div className="mb-8 w-full px-3 lg:mb-0 lg:w-2/5">
+            <p className="mx-auto max-w-md leading-relaxed text-blueGray-400 lg:mx-0 lg:max-w-full lg:pr-32 lg:text-xl">
+              Helping you <strong>maximize</strong> operations management with
+              digitization
+            </p>
+          </div>
+          <div className="mb-8 w-full px-3 lg:mb-0 lg:w-1/5">
+            <p className="mb-2 font-heading font-bold lg:mb-4 lg:text-lg">
+              Office
+            </p>
+            <p className="text-blueGray-400 lg:text-lg">
+              359 Hidden Valley Road, NY
+            </p>
+          </div>
+          <div className="mb-8 w-full px-3 lg:mb-0 lg:w-1/5">
+            <p className="mb-2 font-heading font-bold lg:mb-4 lg:text-lg">
+              Contacts
+            </p>
+            <p className="text-blueGray-400 lg:text-lg">(+01) 234 568</p>
+            <p className="text-blueGray-400 lg:text-lg">contact@monst.com</p>
           </div>
         </div>
-      </div>
+        <div className="flex flex-col items-center lg:flex-row lg:justify-between">
+          <p className="text-sm text-blueGray-400">
+            © 2021. All rights reserved. Designed by{" "}
+            <Link href="#" className="text-blue-400">
+              Alithemes.com
+            </Link>
+          </p>
+          <div className="order-first -mx-2 mb-4 lg:order-last lg:mb-0">
+            <Link href="#" className="inline-block px-2">
+              <Image src={facebookSvg as string} alt="facebook-logo" />
+            </Link>
+            <Link href="#" className="inline-block px-2">
+              <Image src={twitterSvg as string} alt="facebook-logo" />
+            </Link>
+            <Link href="#" className="inline-block px-2">
+              <Image src={instagramSvg as string} alt="facebook-logo" />
+            </Link>
+          </div>
+        </div>
+      </motion.div>
     </section>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,7 @@ import {
   jumpMotion,
 } from "~/utils/animation";
 import HowWeWork from "~/components/how-we-work";
+import Footer from "~/components/footer";
 
 interface Product {
   title: string;
@@ -518,74 +519,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
-      {/* Footer */}
-      <section className="py-20">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{
-            duration: 1,
-          }}
-          className="container"
-        >
-          <div className="-mx-3 mb-12 flex flex-wrap text-center lg:mb-20 lg:text-left">
-            <div className="mb-6 w-full px-3 lg:mb-0 lg:w-1/5">
-              <Link
-                href="/"
-                className="mx-auto inline-block text-3xl font-semibold leading-none lg:mx-0"
-              >
-                <Image
-                  className="h-10"
-                  src="https://wp.alithemes.com/html/monst/assets/imgs/logos/monst-logo.svg"
-                  height={40}
-                  width={125.8}
-                  alt="logo"
-                />
-              </Link>
-            </div>
-            <div className="mb-8 w-full px-3 lg:mb-0 lg:w-2/5">
-              <p className="mx-auto max-w-md leading-relaxed text-blueGray-400 lg:mx-0 lg:max-w-full lg:pr-32 lg:text-xl">
-                Helping you <strong>maximize</strong> operations management with
-                digitization
-              </p>
-            </div>
-            <div className="mb-8 w-full px-3 lg:mb-0 lg:w-1/5">
-              <p className="mb-2 font-heading font-bold lg:mb-4 lg:text-lg">
-                Office
-              </p>
-              <p className="text-blueGray-400 lg:text-lg">
-                359 Hidden Valley Road, NY
-              </p>
-            </div>
-            <div className="mb-8 w-full px-3 lg:mb-0 lg:w-1/5">
-              <p className="mb-2 font-heading font-bold lg:mb-4 lg:text-lg">
-                Contacts
-              </p>
-              <p className="text-blueGray-400 lg:text-lg">(+01) 234 568</p>
-              <p className="text-blueGray-400 lg:text-lg">contact@monst.com</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center lg:flex-row lg:justify-between">
-            <p className="text-sm text-blueGray-400">
-              © 2021. All rights reserved. Designed by{" "}
-              <Link href="#" className="text-blue-400">
-                Alithemes.com
-              </Link>
-            </p>
-            <div className="order-first -mx-2 mb-4 lg:order-last lg:mb-0">
-              <Link href="#" className="inline-block px-2">
-                <Image src={facebookSvg as string} alt="facebook-logo" />
-              </Link>
-              <Link href="#" className="inline-block px-2">
-                <Image src={twitterSvg as string} alt="facebook-logo" />
-              </Link>
-              <Link href="#" className="inline-block px-2">
-                <Image src={instagramSvg as string} alt="facebook-logo" />
-              </Link>
-            </div>
-          </div>
-        </motion.div>
-      </section>
+      <Footer />
     </>
   );
 };
